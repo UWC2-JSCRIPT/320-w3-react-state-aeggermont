da231c1dd4bb9564c7e8fee9dd944d95133229e1
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-13 21:10:05 -0800

    improved layout

--- ecommerce-ui/src/VacationRental/VacationRental.jsx
+++ ecommerce-ui/src/VacationRental/VacationRental.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, Component }  from 'react';
 import PropTypes from 'prop-types';
 import './vacation-rental.css';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import Button from "@mui/material/Button";
 
-function VacationRental(props) {
+function VacationRentalTest(props) {
     return (
 
         <div> 
@@ -19,10 +25,175 @@ function VacationRental(props) {
     )
 }
 
+class VacationRental extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('>>> VacationRental <<<');
+        console.log(props);
+        this.state = {
+            reserved: false
+        }
+
+        this.title = props.title;
+        this.image = props.image;
+        this.houseType = props.houseType;
+        this.location = props.location;
+        this.payment = props.payment;
+        this.host = props.host;
+        this.rating = props.rating;
+        this.propertyId = props.propertyId;       
+    }
+
+
+    handleReservChange = (reservedStatus) => {
+        console.log('>>> handleReservChange <<<');
+        //console.log(this.state);
+        
+        this.setState(() => ({
+            reserved: reservedStatus,
+            title: this.title,
+            image:  this.image,
+            houseType: this.houseType,
+            location: this.location,
+            payment: this.payment,
+            host: this.host,
+            propertyId: this.propertyId
+        }),
+        () => this.props.updateReservations(this.state));
+
+        // this.setState( {reserved: true});
+
+        //console.log(this.state);
+        // this.props.handleTextChange(this.state);
+       
+        /*
+        setReservation( () => ({
+            reserved: true,
+            title: props.title,
+            image:  props.image,
+            houseType: props.houseType,
+            location: props.location,
+            payment: props.payment,
+            host: props.host,
+        }));*/
+    }
+
+    render() {
+        return (
+            <div style={{margin: '2%'}}>
+                <Card sx={{ width: '330px', heigh: '270px' }}  xs={12} sm={4} md={3} lg={3} >
+                <CardMedia
+                    component="img"
+                    height="200"
+                    image={this.image}
+                    alt="Chevrolet"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h7" component="div">
+                    { this.title }
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    { this.houseType }
+                    </Typography> 
+                    <Typography variant="body2" color="text.secondary">
+                    { this.location.city },  { this.location.country }
+                    </Typography> 
+                    <Typography variant="body2" color="text.secondary">
+                    { this.host.name },  { this.rating.stars } - { this.rating.reviews }
+                    </Typography> 
+        
+                </CardContent>
+                <CardActions>
+                    <Button onClick= {()=> this.handleReservChange(true)} size="small">Reserve</Button>
+                    <Button onClick= {()=> this.handleReservChange(false)} size="small">Remove</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+                </Card>
+            </div>
+        );
+    }   
+    
+}
+
+
+function  VacationRentalOld(props) {
+    const title = props.title;
+    const image = props.image;
+    const houseType = props.houseType;
+    const location = props.location;
+    const payment = props.payment;
+    const host = props.host;
+    const rating = props.rating;
+
+    const [property, setReservation] = useState({
+        reserved: false,
+        title: "",
+        image: "",
+        payment: ""
+    });
+
+    const handleReservChange = (e) => {
+        console.log('>>> handleReservChange <<<');
+        if (property.reserved ) {
+            console.log(property);
+        }
+
+        setReservation( () => ({
+            reserved: true,
+            title: props.title,
+            image:  props.image,
+            houseType: props.houseType,
+            location: props.location,
+            payment: props.payment,
+            host: props.host,
+        }));
+
+        
+    }
+  
+    return (
+      <div style={{margin: '2%'}}>
+        <Card sx={{ width: '330px', heigh: '270px' }}  xs={12} sm={4} md={3} lg={3} >
+          <CardMedia
+            component="img"
+            height="200"
+            image={image}
+            alt="Chevrolet"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h7" component="div">
+               { title }
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              { houseType }
+            </Typography> 
+            <Typography variant="body2" color="text.secondary">
+              { location.city },  { location.country }
+            </Typography> 
+            <Typography variant="body2" color="text.secondary">
+              { host.name },  { rating.stars } - { rating.reviews }
+            </Typography> 
+
+          </CardContent>
+          <CardActions>
+            <Button onClick= {()=> handleReservChange()} size="small">Reserve</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+}
+
+VacationRentalTest.propTypes = {
+    title: PropTypes.string.isRequired,
+    houseType: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    location: PropTypes.object.isRequired,
+    payment: PropTypes.object.isRequired,
+    host: PropTypes.object.isRequired,
+    rating: PropTypes.object.isRequired
+}    
+
 export default VacationRental;
 
-        {/*
-            <div className='vacation-rental-container'>
-                <span> { props.property }</span>
-            </div>
-        */}
